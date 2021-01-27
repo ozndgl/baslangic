aa327ecd9ff621770e5c1217fb7c656afd349ccb
Ozan Doğulu <40608249+ozndgl@users.noreply.github.com>
2021-01-28 01:35:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/baslangic/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.baslangic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
